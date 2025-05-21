326fc9c4bc2fdbe2234eb1735b466eaeebdd0132
DinhLong <longnguyendinh120505@gmail.com>
2025-05-21 08:57:24 +0700

    feat: create entity

--- src/main/java/com/mm/mm/entity/Item.java
+++ src/main/java/com/mm/mm/entity/Item.java
@@ -1,4 +1,87 @@
 package com.mm.mm.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.time.LocalDate;
+
+@Entity
 public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String itemID;
+    private String title;
+    private String description;
+    private LocalDate uploadDate;
+    private String categoryID;
+    private String uploaderID;
+    private String ApproverID;
+    private String status;
+
+
+    public String getItemID() {
+        return itemID;
+    }
+
+    public void setItemID(String itemID) {
+        this.itemID = itemID;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getUploadDate() {
+        return uploadDate;
+    }
+
+    public void setUploadDate(LocalDate uploadDate) {
+        this.uploadDate = uploadDate;
+    }
+
+    public String getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(String categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getUploaderID() {
+        return uploaderID;
+    }
+
+    public void setUploaderID(String uploaderID) {
+        this.uploaderID = uploaderID;
+    }
+
+    public String getApproverID() {
+        return ApproverID;
+    }
+
+    public void setApproverID(String approverID) {
+        ApproverID = approverID;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
