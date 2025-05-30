ac197dd631d58c0377c2738aeb15edbd817c71b7
DinhLong <longnguyendinh120505@gmail.com>
2025-05-30 23:26:38 +0700

    feat: create category entity

---
+++ src/main/java/com/mm/mm/entity/Category.java
@@ -0,0 +1,35 @@
+package com.mm.mm.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "CategoryID")
+    private Integer categoryID;
+
+    @Column(name = "CategoryName")
+    private String categoryName;
+
+    // Getters and Setters
+
+    public Integer getCategoryID() {
+        return categoryID;
+    }
+
+    public void setCategoryID(Integer categoryID) {
+        this.categoryID = categoryID;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
+
