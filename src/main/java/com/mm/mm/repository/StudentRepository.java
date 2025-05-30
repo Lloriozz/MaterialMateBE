90fbfad5ea9eba43a1ab24132d1b7136ca054a6a
DinhLong <longnguyendinh120505@gmail.com>
2025-05-30 23:48:27 +0700

    feat: adjust the attributes

--- src/main/java/com/mm/mm/repository/StudentRepository.java
+++ src/main/java/com/mm/mm/repository/StudentRepository.java
@@ -2,11 +2,11 @@ package com.mm.mm.repository;
 
 import com.mm.mm.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+@Repository
+public interface StudentRepository extends JpaRepository<Student, String> {
+    boolean existsByUsername(String username);
 
-public interface StudentRepository extends JpaRepository<Student, Long> {
-    boolean existsByUserName(String userName);
-    Student findByUserName(String userName);
-
+    Student findByUsername(String username);
 }
