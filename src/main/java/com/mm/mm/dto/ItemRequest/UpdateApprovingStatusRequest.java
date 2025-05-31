af4356bc0473fb3ab964b6ee257d81f7adfc518b
DinhLong <longnguyendinh120505@gmail.com>
2025-05-31 14:57:05 +0700

    feat: add request for update status

---
+++ src/main/java/com/mm/mm/dto/ItemRequest/UpdateApprovingStatusRequest.java
@@ -0,0 +1,13 @@
+package com.mm.mm.dto.ItemRequest;
+
+public class UpdateApprovingStatusRequest {
+    private String approvingStatus;
+
+    public String getApprovingStatus() {
+        return approvingStatus;
+    }
+
+    public void setApprovingStatus(String approvingStatus) {
+        this.approvingStatus = approvingStatus;
+    }
+}
