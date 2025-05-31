670289533674edd67a5271377a93542fecad0aff
DinhLong <longnguyendinh120505@gmail.com>
2025-05-31 16:30:18 +0700

    feat: add get all category

--- src/main/java/com/mm/mm/controller/CategoryController.java
+++ src/main/java/com/mm/mm/controller/CategoryController.java
@@ -2,8 +2,11 @@ package com.mm.mm.controller;
 
 import java.util.List;
 
+import com.mm.mm.entity.Category;
 import com.mm.mm.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,15 +14,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 @RequestMapping("/categories")
 @CrossOrigin(origins = "*") // Optional: only if you're working with frontend dev
 public class CategoryController {
+    private static final Logger logger = LoggerFactory.getLogger(CategoryController.class);
 
     @Autowired
     private CategoryService categoryService;
 
+    @GetMapping("/all")
+    public ResponseEntity<?> getAllCategories() {
+        try {
+            logger.info("Received request to get all categories");
+            List<Category> categories = categoryService.getAllCategories();
+            return ResponseEntity.ok(categories);
+        } catch (Exception e) {
+            logger.error("Error getting all categories: ", e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("An error occurred while getting categories");
+        }
+    }
 
 }
--- src/main/java/com/mm/mm/service/CategoryService.java
+++ src/main/java/com/mm/mm/service/CategoryService.java
@@ -4,14 +4,22 @@ import com.mm.mm.entity.Category;
 import com.mm.mm.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class CategoryService {
+    private static final Logger logger = LoggerFactory.getLogger(CategoryService.class);
+
     @Autowired
     private CategoryRepository categoryRepository;
 
+    public List<Category> getAllCategories() {
+        logger.info("Getting all category records");
+        return categoryRepository.findAll();
+    }
+
+    // You can add other business logic methods here
 
 }
-
