e09ac88fadccf4ca4d2909b7bc15e480839d14e0
DinhLong <longnguyendinh120505@gmail.com>
2025-05-31 16:30:05 +0700

    feat: add get all admin

--- src/main/java/com/mm/mm/service/AdminService.java
+++ src/main/java/com/mm/mm/service/AdminService.java
@@ -4,14 +4,22 @@ import com.mm.mm.entity.Admin;
 import com.mm.mm.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @Service
 public class AdminService {
+    private static final Logger logger = LoggerFactory.getLogger(AdminService.class);
+
     @Autowired
     private AdminRepository adminRepository;
 
+    public List<Admin> getAllAdmins() {
+        logger.info("Getting all admin records");
+        return adminRepository.findAll();
+    }
+
+    // You can add other business logic methods here
 
 }
-
